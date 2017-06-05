485382f3624b5a378e4409225deaf22bc437fb74
Brainbuggged <brainbuggged@gmail.com>
2017-06-05 19:18:20 +0300

    Added

---
+++ calendar/Daily/Daily/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Daily
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(421, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(393, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(421, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(617, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(421, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(386, 121);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Событие:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(184, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 146);
+            this.label2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 210);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.monthCalendar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ calendar/Daily/Daily/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Daily
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string mainEvents = @"Events.dat";
+
+
+        public class Event
+        {
+            private DateTime date;
+
+            private string occasion;
+
+            public DateTime Date
+            {
+                get { return date; }
+                set { date = value; }
+            }
+            public string Occasion
+            {
+                get { return occasion; }
+                set { occasion = value; }
+            }
+            public void Read(BinaryReader reader)
+            {
+                long data = reader.ReadInt64();
+                date = DateTime.FromBinary(data);
+                occasion = reader.ReadString();
+                dates.Add(date);
+            }
+
+            public void Write(BinaryWriter writer)
+            {       
+                writer.Write(date.Date.ToBinary());
+                writer.Write(occasion);     
+            }
+        }
+        List<Event> events = new List<Event>();
+        static  List<DateTime> dates = new List<DateTime>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "Ежедневник 1.0";
+            BinaryReader reader = new BinaryReader(File.Open(mainEvents, FileMode.Open));
+            while (reader.PeekChar() != -1)
+            {
+                Event newEvent = new Event();
+                newEvent.Read(reader);
+                events.Add(newEvent);
+               
+            }
+            
+                foreach (Event o in events)
+                {
+                   monthCalendar1.AddBoldedDate(o.Date);
+                   monthCalendar1.UpdateBoldedDates();
+
+                }
+                monthCalendar1_DateSelected(null, new DateRangeEventArgs(DateTime.Now.Date, DateTime.Now.Date));
+                monthCalendar1.UpdateBoldedDates();
+                reader.Close();
+            
+
+        }
+       
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            FileStream fileStream = File.Open(mainEvents, FileMode.Open);
+            fileStream.SetLength(0);
+            fileStream.Close();
+            BinaryWriter writer = new BinaryWriter(File.Open(mainEvents, FileMode.OpenOrCreate));
+                foreach (Event j in events)
+                {
+                    j.Write(writer);
+                }      
+                events.Clear();
+                dates.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox1.Text)==false)
+            {
+                if (dates.Contains(dateTimePicker1.Value.Date)==false)
+                {
+                    Event newEvent = new Event();
+                    newEvent.Date = dateTimePicker1.Value.Date;
+                    newEvent.Occasion = textBox1.Text;
+                    events.Add(newEvent);
+                    dates.Add(dateTimePicker1.Value.Date);
+                    monthCalendar1.AddBoldedDate(dateTimePicker1.Value.Date);
+                    monthCalendar1.UpdateBoldedDates();
+                    textBox1.Text = "";
+                }
+                else MessageBox.Show("Выберите другой день. Этот уже занят", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else MessageBox.Show("Заполните поле текста, пожалуйста.", "Ошибка",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            
+            bool found = false;
+            foreach (Event o in events)
+            {
+                if (monthCalendar1.SelectionRange.Start.Date == o.Date.Date)
+                {
+                    label2.Text = o.Occasion;               
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                label2.Text = "Событие отстутсвует.";           
+            }  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < events.Count; i++)
+            {
+                if (monthCalendar1.SelectionRange.Start.Date == events[i].Date.Date)
+                {               
+                        monthCalendar1.RemoveBoldedDate(monthCalendar1.SelectionRange.Start.Date);
+                        monthCalendar1.UpdateBoldedDates();
+                        events.RemoveAt(i);
+                       
+                        label2.Text = "Событие удалено!";
+                }
+            }
+        }
+    }
+}
+
